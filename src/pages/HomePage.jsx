c2daea2758e720e3d042a87bc2e8172eec71d3bf
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-28 22:11:08 +0700

    feat: add home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import { Header } from "../components/Header"
+
+
+export const HomePage = () => {
+
+  return (
+    <div>
+      <Header />
+      <div>
+        <h1 className="flex justify-center py-20 text-4xl font-bold">You need to login</h1>
+      </div>
+    </div>
+  )
+}
