d4d36fc3b5a1d32a3d9bd558ac922190c92246d9
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-28 22:10:32 +0700

    feat: add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { CgProfile } from "react-icons/cg";
+import { MdLogin } from "react-icons/md";
+import { LuBugOff } from "react-icons/lu";
+
+export const Header = () => {
+  return (
+    <div>
+        <nav className="flex justify-between h-20 w-screen border border-b-2 px-20">
+            <div className="flex items-center gap-5 px-10">
+                <Link to="/">
+                    <span className="text-3xl font-bold"><LuBugOff/></span>
+                </Link>
+            </div>
+            <div className="flex items-center gap-10 px-10">
+                <div className="flex items-center gap-3">
+                    <MdLogin className="w-6 h-6" />
+                    <Link to="/login">
+                        <span className="text-xl">Login</span>
+                    </Link>
+                </div>
+                <CgProfile className="w-8 h-8" />
+            </div>
+        </nav>
+    </div>
+  )
+}
