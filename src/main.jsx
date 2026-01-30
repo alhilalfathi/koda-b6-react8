e5a9e1b1ee5d145cc254f410630f8fd9143cf0b8
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-30 15:53:06 +0700

    feat: wrap Router with AuthProvider useContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './assets/css/style.css'
 import { Router } from './Router.jsx'
+import { AuthProvider } from './components/Auth.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Router />
-  </StrictMode>,
+  
+    <AuthProvider>
+      <Router />
+    </AuthProvider>
+  
+    
+  
 )
