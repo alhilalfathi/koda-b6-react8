709e2732978d3589a1c898106c31546a40b20822
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-30 15:51:14 +0700

    feat: add useContext to check login credential

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,31 @@
+import { useContext } from "react"
 import { Header } from "../components/Header"
+import { AuthContext } from "../components/Auth"
+
 
 
 export const HomePage = () => {
+const auth = useContext(AuthContext)
+const {isLogin, logout} = auth
 
   return (
     <div>
-      <Header />
-      <div>
-        <h1 className="flex justify-center py-20 text-4xl font-bold">You need to login</h1>
-      </div>
+      {isLogin ? (
+        <div >
+          <Header status={"Logged in"} />
+          <div className="flex justify-center py-60 flex-col items-center gap-5">
+            <h1 className="text-2xl font-bold">Welcome, you are logged in</h1>
+            <button onClick={logout} className="bg-black text-white p-3 rounded">Logout</button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <Header status={"Login"} />
+          <div className="flex justify-center py-60">
+            <h1 className="text-2xl font-bold">Please login</h1>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
