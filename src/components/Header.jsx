97ca420e79082acc708ce7f98f9b232570a5aa0e
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-30 15:49:28 +0700

    fix: change login span into Header props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
 import { Link } from "react-router-dom";
-import { GiHamburgerMenu } from "react-icons/gi";
 import { CgProfile } from "react-icons/cg";
 import { MdLogin } from "react-icons/md";
 import { LuBugOff } from "react-icons/lu";
 
-export const Header = () => {
+export const Header = (props) => {
   return (
     <div>
         <nav className="flex justify-between h-20 w-screen border border-b-2 px-20">
@@ -17,7 +16,7 @@ export const Header = () => {
                 <div className="flex items-center gap-3">
                     <MdLogin className="w-6 h-6" />
                     <Link to="/login">
-                        <span className="text-xl">Login</span>
+                        <span className="text-xl">{props.status}</span>
                     </Link>
                 </div>
                 <CgProfile className="w-8 h-8" />
